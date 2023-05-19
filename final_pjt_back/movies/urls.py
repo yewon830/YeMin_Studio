1940279dbc06da4df77a04f78d0038fd6891c17b
권민재 <knk598500@gmail.com>
2023-05-19 17:38:24 +0900

    add movie_Search

--- final_pjt_back/movies/urls.py
+++ final_pjt_back/movies/urls.py
@@ -4,5 +4,6 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.movie_home),  # 영화 추천 웹 페이지
     path('<int:movie_pk>/', views.movie_detail),  # 영화 상세 정보 팝업
+    path('<str:sw_value>/', views.movie_search), # 검색어에 따른 페이지
     # path('page/<int:page_number>/', views.movie_page),  # 페이지 번호로 페이지로 이동
 ]
--- final_pjt_back/movies/views.py
+++ final_pjt_back/movies/views.py
@@ -1,18 +1,14 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from .models import Movie
 from .serializers import MovieDetailSerializer, MovieSerializer
 import json
 import random
-# Create your views here.
-@api_view(['GET'])
-def movie_detail(request,movie_pk):
-    movie  = get_object_or_404(Movie, pk = movie_pk)    
-    movie_serializer = MovieDetailSerializer(movie)
-    return Response(movie_serializer.data)
 
+# Create your views here.
 @api_view(['GET'])
 def movie_home(request):
     movie_list = list(Movie.objects.all())
@@ -24,6 +20,22 @@ def movie_home(request):
     movie_data = MovieSerializer(page_obj, many=True)
     return Response(movie_data.data)
 
+@api_view(['GET'])
+def movie_detail(request, movie_pk):
+    movie  = get_object_or_404(Movie, pk = movie_pk)    
+    movie_serializer = MovieDetailSerializer(movie)
+    return Response(movie_serializer.data)
+
+@api_view(['GET'])
+def movie_search(request, sw_value):
+    movie_list = Movie.objects.filter(Q(title__contains=sw_value) | Q(overview__contains=sw_value))
+    
+    paginator = Paginator(movie_list, 30)
+    
+    page_number = 1
+    page_obj = paginator.get_page(page_number)
+    movie_data = MovieSerializer(page_obj, many=True)
+    return Response(movie_data.data)
 
 # def movies(request, page):
 #     movie_list = []
